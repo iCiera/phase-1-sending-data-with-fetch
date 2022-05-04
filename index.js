bd711be7effb3133e4fb17020796474b51c9c743
ciera <cieranicole.m15@gmail.com>
2022-05-03 23:04:44 -0400

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your code here
+
+function submitData(name, email) {
+  const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email,
+      })
+    };
+
+  return fetch('http://localhost:3000/users', configurationObject)
+    .then(response => response.json())
+    .then(user => document.body.innerHTML = user.id)
+    .catch(error => document.body.innerHTML = error.message)
+}
